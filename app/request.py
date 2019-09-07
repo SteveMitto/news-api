e28343b7ae23e02a4031a0265ec06b0f5fa462f9
Steve <mittosteve@gmail.com>
2019-09-07 15:15:53 +0300

    This commit makes a request for the news source articles

--- app/request.py
+++ app/request.py
@@ -1,10 +1,9 @@
 import requests
 from config import Config
-from .models import NewsSource
+from .models import NewsSource,NewsArticle
 
 api_key =Config.API_KEY
 source_url =f'https://newsapi.org/v2/sources?language=en&apiKey={api_key} '
-
 
 def get_sources():
     response = requests.get(source_url)
@@ -29,3 +28,25 @@ def get_sources():
 
     else:
         return None
+def get_source_article(source_id):
+    ''' This function gets articles of a certain news source '''
+    source_articles_url = f'https://newsapi.org/v2/top-headlines?sources={source_id}&apiKey={api_key}'
+
+    response = requests.get(source_articles_url)
+    articles_list=[]
+    if response.status_code == 200:
+        response_json = response.json()
+        articles = response_json['articles']
+
+        for article in articles:
+            author = article.get('author')
+            title = article.get('title')
+            description = article.get('description')
+            image = article.get('image')
+            published = article.get('published')
+            content = article.get('content')
+
+            if image:
+                article_object = NewsArticle(author,title,description,image,published,content)
+                articles_list.append(article_object)
+    return articles_list
