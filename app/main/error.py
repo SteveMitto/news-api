75461a5c2355884a3516d4483cd5f6a4a13d0b1b
Steve <mittosteve@gmail.com>
2019-09-09 18:17:46 +0300

    This commit adds a 404 error page

--- app/main/error.py
+++ app/main/error.py
@@ -3,4 +3,5 @@ from flask import render_template as r_t
 
 @main.app_errorhandler(404)
 def error_page(error):
-    return r_t('four04.html')
+    four =True
+    return r_t('four04.html' ,four = four)
